5bc92c910a4d0747b6436263310d3581cd39c980
Doubzero <131899921+Doubzero@users.noreply.github.com>
2023-08-08 16:08:34 -0400

    Added setOperation function.

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const numberButtons = document.querySelectorAll("[data-number]");
-
+const operatorButtons = document.querySelectorAll("[data-operator");
 const clearButton = document.getElementById("data-clear");
 const windowPreview = document.getElementById("currentPreview");
 const resultWindow = document.getElementById("resultWindow");
 
+//event Listeners
 clearButton.addEventListener("click", clearAll);
-
+//cycle through all objects to write button text to screen.
 numberButtons.forEach((button) =>
   button.addEventListener("click", () => writeToScreen(button.textContent))
 );
-
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+//helper functions
 function writeToScreen(number) {
   if (windowPreview.textContent == "0") {
     windowPreview.textContent = "";
   }
   windowPreview.textContent += number;
 }
-
+function setOperation(operator) {
+  windowPreview.textContent += " " + operator + " ";
+}
 function clearAll() {
   resultWindow.textContent = "0";
   windowPreview.textContent = "0";
